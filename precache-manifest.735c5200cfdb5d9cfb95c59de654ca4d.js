afe607225d35868e8af94ddb29a5882bbcbc991e
Ihor <ihor.volovyk@gmail.com>
2019-11-18 03:30:42 +0200

    Updates

---
+++ precache-manifest.735c5200cfdb5d9cfb95c59de654ca4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd953acbd1973b4939caad75ec805c7",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "1a9bdf3d349745b77513",
+    "url": "/trello-clone/static/css/main.7909459c.chunk.css"
+  },
+  {
+    "revision": "1fea0de4fdda25704046",
+    "url": "/trello-clone/static/js/2.2c280c21.chunk.js"
+  },
+  {
+    "revision": "1a9bdf3d349745b77513",
+    "url": "/trello-clone/static/js/main.1942ccba.chunk.js"
+  },
+  {
+    "revision": "c8409f23391c14b691bb",
+    "url": "/trello-clone/static/js/runtime-main.aaae5460.js"
+  }
+]);
